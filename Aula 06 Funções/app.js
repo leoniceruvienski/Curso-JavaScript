60fd9aeb59eeab0c4e1226372ceb31203ef83617
Finer <finer@pop-os.localdomain>
2021-12-28 22:23:24 -0300

    Aula 06

---
+++ Aula 06 Funções/app.js
@@ -0,0 +1,19 @@
+  function action(){
+      console.log('realiza uma ação')
+  }
+
+  action()
+
+  function action(){
+    return 100
+}
+
+console.log(15 + action())
+
+function sum(number1, number2) {
+    return number1 + number2  
+}
+
+console.log (sum(10,50))
+console.log (sum(2,5))
+
