863bc8edd8c8402e9a4baafbcb77c3936524c45a
Finer <finer@pop-os.localdomain>
2021-12-29 19:07:29 -0300

    Aula 08

---
+++ Aula 08 Operadores de Comparação/app.js
@@ -0,0 +1,24 @@
+if (10==10){
+    console.log('ok')
+}
+if (10=='10'){
+    console.log('ok')
+}
+if (10===10){
+    console.log('ok')
+}
+if (10==='10'){
+    console.log('ok')
+}
+if (5 != 10){
+    console.log('ok')
+}
+if ('verde' != 'amarelo'){
+    console.log('ok')
+}
+if (10 >= 5){
+    console.log('ok')
+}
+if (7 <= 10){
+    console.log('ok')
+}
