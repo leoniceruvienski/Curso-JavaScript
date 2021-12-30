604db4b2312c82d67522987b73b17b7827c2ce2c
Finer <finer@pop-os.localdomain>
2021-12-30 14:34:01 -0300

    Aula 10

---
+++ Aula 10 Operador Ternário/app.js
@@ -0,0 +1,62 @@
+var age = 18
+var result = ''
+
+if (age >= 18) {
+    result = 'ok'
+}else{
+    result = ' não ok'
+}
+console.log(result)
+
+
+var age = 10
+var result = ''
+
+if (age >= 18) {
+    result = 'ok'
+}else{
+    result = 'não ok'
+}
+console.log(result)
+
+ var age = 15
+ var result = (age >= 18)
+ console.log(result)
+
+
+ var age = 18
+ var result = (age >= 18)
+ console.log(result)
+
+//Operador Ternário
+ var age = 18
+ var result = age >= 18 ? 'ok' : 'não ok'
+ // condição ? true : false
+ console.log(result)
+
+//test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
