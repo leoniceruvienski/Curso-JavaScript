390f4ddb46a22ea8d912ef54213fed6dbeb5f642
Leonice <leoniceruvienski@gmail.com>
2022-01-02 20:58:52 -0200

    Aula 13

---
+++ Aula 13 Escopo de Bloco/app.js
@@ -0,0 +1,18 @@
+if (true){
+    var cc = 'BMW'
+}
+ console.log(cc)
+
+
+ var car = ''
+
+ if(true) {
+     carr = 'Palio'
+ }
+ console.log(carr)
+
+ if (true) {
+     let car = "Gol"
+ }
+ console.log(car)
+
