1d05615168191cc00e2236dc81b501821d35d47e
Leonice <leoniceruvienski@gmail.com>
2022-01-03 17:47:45 -0200

    Aula 14

---
+++ Aula 14 Operações em String/app.js
@@ -0,0 +1,39 @@
+var person = {
+    name: 'Leonice',
+    heigth: '1.64',
+    sayHello: function(){
+        console.log('Hello')
+    },
+}
+person.sayHello()
+
+
+var text= ' Leonice '
+var value = text.length
+console.log(value)
+
+
+var text= 'Leonice'
+var value = text[0]
+console.log(value)
+
+
+var text= 'Leonice'
+var value = text.replace('o', '@')
+console.log(value)
+
+
+var text= 'Leonice'
+var value = text.toUpperCase()
+console.log(value)
+
+
+var text= 'Leonice'
+var value = text.toLowerCase()
+console.log(value)
+
+
+var text= 'Leonice Ruvienski'
+var value = text.split(' ')
+console.log(value)
+
