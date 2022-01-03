cb0e73f53d2e916c5ffed27f89d64e26c7122980
Leonice <leoniceruvienski@gmail.com>
2022-01-03 17:22:17 -0200

    Aula 15

---
+++ Aula 15 Operações em Números/app.js
@@ -0,0 +1,23 @@
+var number = 10.3
+var newValue = number
+console.log(newValue)
+
+
+var number = 10.3
+var newValue = number.toString()
+console.log(newValue)
+
+
+var number = 10.3
+var newValue = Math.trunc(number)
+console.log(newValue)
+
+
+var number = 10.3
+var newValue = Math.ceil(number)
+console.log(newValue)
+
+
+var number = 10.3
+var newValue = Math.round(number)
+console.log(newValue)
