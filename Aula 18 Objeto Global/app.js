0fbafbe1027da5e6cbaa8519df547650bec5a1aa
Leonice <leoniceruvienski@gmail.com>
2022-01-06 14:00:08 -0200

    Aula 18

---
+++ Aula 18 Objeto Global/app.js
@@ -0,0 +1,8 @@
+alert('Bom Dia ;)')
+// Não precisa por o window antes do alert, pois funciona igual
+
+console.log(window.innerHeight)
+
+console.log(window.innerWidth)
+
+window.location.href = "http://google.com"
