b99834a8ac709803ac09458c92c74379b4b9bff0
Leonice <leoniceruvienski@gmail.com>
2022-01-09 16:21:18 -0200

    Aula 23

---
+++ Aula 23 Alterando Conteúdo/app.js
@@ -0,0 +1,37 @@
+// var app = document.querySelector('div#app')
+console.log(app.outerHTML)
+
+
+
+var app = document.querySelector('div#app')
+console.log(app.innerHTML)
+
+
+
+
+var app = document.querySelector('div#app')
+console.log(app.innerText)
+
+
+
+var app = document.querySelector('div#app')
+app.outerHTML = 'Olá'
+
+
+
+var app = document.querySelector('div#app')
+app.innerHTML = 'Olá'
+
+
+var app = document.querySelector('div#app')
+app.innerText = 'Olá'
+
+
+
+var app = document.querySelector('div#app')
+app.innerHTML = '<p>Testando 123</p>'
+
+
+
+var app = document.querySelector('div#app')
+app.innerText = '<p>Testando 123</p>'
