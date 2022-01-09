8c5a353d56b0cb5cbfa72de376b5157a1b88eea5
Leonice <leoniceruvienski@gmail.com>
2022-01-09 12:31:02 -0200

    Aula 22

---
+++ Aula 22 Alterando Estilos/app.js
@@ -0,0 +1,11 @@
+var element = document.querySelector('body')
+
+element.style.background = '#000'
+
+
+
+ var element = document.querySelector('div#app')
+
+ element.style.backgroundColor = '#000'
+ element.style.height = '200px'
+ element.style.width = '200px'
