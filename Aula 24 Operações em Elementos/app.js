bebf224b25002d39e7282338222334846bb528cb
Leonice <leoniceruvienski@gmail.com>
2022-01-10 16:08:29 -0200

    Aula 25

---
+++ Aula 24 Operações em Elementos/app.js
@@ -0,0 +1,27 @@
+var app = document.querySelector('div#app')
+var link = document.createElement('a')
+link.href = "http://google.com"
+
+
+
+
+var app = document.querySelector('div#app')
+var link = document.createElement('a')
+var textLink = document.createTextNode('Visitar Google')
+link.setAttribute('href', 'http://google.com')
+link.appendChild(textLink)
+app.appendChild(link)
+
+
+
+
+var app = document.querySelector('div#app')
+var text = document.querySelector('div#app  p')
+app.removeChild(text)
+
+
+
+
+
+
+ 
