07baf9afa7ce3dc4e36a2b32fea89eef6fae0bed
Leonice <leoniceruvienski@gmail.com>
2022-01-10 17:43:07 -0200

    Aula 25

---
+++ Aula 25 Intervalo e Timeout/app.js
@@ -0,0 +1,21 @@
+window.setInterval(function (){
+    console.log('executou')
+}, 3000)
+
+
+
+function doAction () {
+    console.log('executou')
+}
+
+setInterval(doAction, 3000)
+
+
+
+
+function doAction () {
+    console.log('executou')
+}
+
+setTimeout(doAction, 3000)
+    
