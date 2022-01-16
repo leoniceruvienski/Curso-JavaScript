ba8a44b8c31dd9e56f9633f9288465dc1a3603bc
Leonice <leoniceruvienski@gmail.com>
2022-01-16 16:31:53 -0200

    Aula 32

---
+++ Aula 32 Buscando Informações/app.js
@@ -0,0 +1,25 @@
+var submitButton = document.querySelector('#app form button')
+var zipCodeField = document.querySelector('#app form input')
+var content = document.querySelector('#app main') 
+
+submitButton.addEventListener('click', run)
+
+function run(event) {
+    event.preventDefault()
+
+    var zipCode = zipCodeField.value
+
+    zipCode = zipCode.replace(' ', '')
+    zipCode = zipCode.replace('.', '')
+    zipCode = zipCode.trim()
+
+    axios
+    .get('https://viacep.com.br/ws/' + zipCode + '/json/')
+    .then(function(response){
+        console.log(response.data)
+    })
+    .catch(function (error){
+        console.log(error)
+    })
+
+}
