4b89d71cd93bdc5debd578cf983b931911cc5d22
Leonice <leoniceruvienski@gmail.com>
2022-01-17 09:00:03 -0200

    Aula 33

---
+++ Aula 33 Renderizando Informações/app.js
@@ -0,0 +1,70 @@
+var submitButton = document.querySelector('#app form button')
+var zipCodeField = document.querySelector('#app form input')
+var content = document.querySelector('#app main') 
+
+submitButton.addEventListener('click', run)
+
+function run(event) {
+    event.preventDefault()
+
+    var zipCode = zipCodeField.value
+
+    zipCode = zipCode.replace(' ', '')
+    zipCode = zipCode.replace('.', '')
+    zipCode = zipCode.trim()
+
+    axios
+    .get('https://viacep.com.br/ws/' + zipCode + '/json/')
+    .then(function(response){
+     var line = document.createElement('p')  
+     var text = document.createTextNode(response.data.logradouro)
+     
+     line.appendChild(text)
+     content.appendChild(line)  
+    })
+    .catch(function (error){
+        console.log(error)
+    })
+
+}
+
+
+
+
+
+var submitButton = document.querySelector('#app form button')
+var zipCodeField = document.querySelector('#app form input')
+var content = document.querySelector('#app main') 
+
+submitButton.addEventListener('click', run)
+
+function run(event) {
+    event.preventDefault()
+
+    var zipCode = zipCodeField.value
+
+    zipCode = zipCode.replace(' ', '')
+    zipCode = zipCode.replace('.', '')
+    zipCode = zipCode.trim()
+
+    axios
+    .get('https://viacep.com.br/ws/' + zipCode + '/json/')
+    .then(function(response){
+        content.innerHTML = ''
+        createLine(response.data.logradouro)
+        createLine(response.data.localidade + '/' + response.data.uf)
+        createLine(response.data.bairro)  
+    })
+    .catch(function (error){
+        console.log(error)
+    })
+   
+}
+function createLine(text) {
+    var line = document.createElement('p')  
+    var text = document.createTextNode(text)
+
+line.appendChild(text)
+content.appendChild(line) 
+}
+
